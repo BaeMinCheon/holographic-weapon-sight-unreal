4595ce20ffbadd091e1cf81d7e9d76c25c633e0b
baemincheon <baemincheon@gmail.com>
2023-12-17 23:13:52 +0900

    create new project from the template Blank without starter contents in UnrealEngine 5.3.2

---
+++ holographics/Intermediate/Build/Win64/x64/holographicsEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/qmffk/source/repos/holographic-weapon-sight-unreal/holographics/Intermediate/Build/Win64/x64/holographicsEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ holographics/Intermediate/Build/Win64/x64/holographicsEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32543
+#include "C:/Users/qmffk/source/repos/holographic-weapon-sight-unreal/holographics/Intermediate/Build/Win64/x64/holographicsEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h"
